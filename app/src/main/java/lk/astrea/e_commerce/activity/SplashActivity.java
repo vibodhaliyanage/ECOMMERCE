dac0fa4c08c8e6491a06606854543d7068d48543
vibodha liyanage <vibodaliyanage274@gmail.com>
2026-02-12 18:16:42 +0530

    Implement navigation drawer and splash screen layout; update themes and colors

---
+++ app/src/main/java/lk/astrea/e_commerce/activity/SplashActivity.java
@@ -0,0 +1,79 @@
+package lk.astrea.e_commerce.activity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+
+import lk.astrea.e_commerce.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().setDecorFitsSystemWindows(false);
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.systemBars() | WindowInsets.Type.navigationBars());
+            } else {
+                getWindow().setFlags(
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN
+                );
+            }
+        }
+
+        setContentView(R.layout.activity_splash);
+        ImageView imageView = findViewById(R.id.splashLogo);
+//        Picasso.get()
+//                .load(R.drawable.ic_launcher_foreground)
+//                .resize(300,300)
+//                .into(imageView);
+
+        Glide.with(this)
+                .asBitmap()
+                .load(R.drawable.ic_launcher_foreground)
+                .override(400)
+                .into(imageView);
+
+        new Handler(Looper.getMainLooper())
+                .postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
+                    }
+                }, 1000);
+
+        new Handler(Looper.getMainLooper())
+                .postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        findViewById(R.id.progressBar).setVisibility(View.INVISIBLE);
+                        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 5000);
+
+    }
+}
